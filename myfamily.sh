c5d99a65c2cb2b34bf902278746afc92b90b9a36
GreyCoderK <leshodaimekossonou@gmail.com>
2019-06-06 12:31:10 +0200

    myfamily

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '. [] | select(.id == $HERO_ID) | .connections.relatives' | sed 's/^"\(.*\)"$/\1/'
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson id "$HERO_ID" '. [] | select(.id == $id) | .connections.relatives' | sed 's/^"\(.*\)"$/\1/'
