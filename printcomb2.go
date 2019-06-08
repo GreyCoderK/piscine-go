91e71ef88cca42bf9c14eec382c5c79091c8bd79
ARMEL REAL <armelreal@MacBook-Pro-de-ARMEL.local>
2019-06-08 15:49:34 +0000

    printcomb2

---
+++ printcomb2.go
@@ -0,0 +1,34 @@
+package piscine
+
+import (
+    "fmt"
+    "strconv"
+)
+
+func PrintComb() {
+	for i:=0; i<99; i++{ 
+    		for j:=i+1; j<100; j++{
+			if i == 0 && j == 1 {
+				fmt.Print("")
+			}else{
+				fmt.Print(", ")
+			}
+			if i < 10 {
+				ch := strconv.Itoa(0)
+				ch += strconv.Itoa(i)
+				fmt.Print(ch," ")
+             		}else{
+				fmt.Print(i," ")
+			}
+			
+			if j < 10 {
+                                ch := strconv.Itoa(0)
+                                ch += strconv.Itoa(j)
+                                fmt.Print(ch)
+                        }else{ 
+                                fmt.Print(j)
+                        }
+        	}
+    	}
+    }
+}
