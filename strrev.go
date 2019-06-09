80c41c162c5ae0da944155be537e3eae9e242aaf
ARMEL REAL <armelreal@MacBook-Pro-de-ARMEL.local>
2019-06-09 02:58:37 +0000

    strrev

---
+++ strrev.go
@@ -0,0 +1,9 @@
+package piscine
+
+func StrRev(s string) string {
+	chars := []rune(s)
+	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
+		chars[i], chars[j] = chars[j], chars[i]
+	}
+	return string(chars)
+}
