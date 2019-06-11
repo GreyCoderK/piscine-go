0d42003f5ed530a06a92e6a4f3345ebca67c1280
GreyCoderK <46757795+GreyCoderK@users.noreply.github.com>
2019-06-11 02:51:32 +0200

    Update raid1b.go

--- raid1b.go
+++ raid1b.go
@@ -3,29 +3,33 @@ package piscine
 import "fmt"
 
 func Raid1b(x, y int){
-	for i := 0; i < y; i++ {
-		for j := 0; j < x; j++ {
-			if i == 0 || i == y-1 {
-				if j == 0 && i==0 || j == x-1 && i == y-1 {
-					if i == y - 1 && x == 1 || i == 0 && j == x - 1 && y == 1 || y == x && x == 1 {
+	if x == y && y == 1 {
+		fmt.Print("\\")
+	}else{
+		for i := 0; i < y; i++ {
+			for j := 0; j < x; j++ {
+				if i == 0 || i == y-1 {
+					if j == 0 && i==0 || j == x-1 && i == y-1 {
+						if i == y - 1 && x == 1 || i == 0 && j == x - 1 && y == 1 {
+							fmt.Print("\\")
+						}else{
+							fmt.Print("/")
+						}
+					}else if j == x-1 && i==0 || j == 0 && i == y-1 {
 						fmt.Print("\\")
-					}else{
-						fmt.Print("/")
+					} else {
+						fmt.Print("*")
 					}
-				}else if j == x-1 && i==0 || j == 0 && i == y-1 {
-					fmt.Print("\\")
 				} else {
-					fmt.Print("*")
+					if j == 0 || j == x-1 {
+						fmt.Print("*")
+					} else {
+						fmt.Print(" ")
+					}
 				}
-			} else {
-				if j == 0 || j == x-1 {
-					fmt.Print("*")
-				} else {
-					fmt.Print(" ")
+				if j == x-1 {
+					fmt.Print("\n")
 				}
-			}
-			if j == x-1 {
-				fmt.Print("\n")
 			}
 		}
 	}
