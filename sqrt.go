7d3516aaea04f51980197de7d6cd68ebfa6ca48e
GreyCoderK <leshodaimekossonou@gmail.com>
2019-06-12 19:34:57 +0200

    quest 04

--- sqrt.go
+++ sqrt.go
@@ -1,7 +1,17 @@
 package piscine
 
-import "math"
-
-func Sqrt(nb int) float64 {
-	return math.Sqrt(float64(nb))
+func Sqrt(nb int) {
+	if nb < 0 {
+		return 0
+	}
+	for i := 0; i < 101; i++ {
+		if nb == i*i {
+			nb = i
+			break
+		} else if i*i > nb {
+			nb = 0
+			break
+		}
+	}
+	return nb
 }
