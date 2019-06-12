2bf90f7c3f54e4b7bfcbf61175863d866aeea502
GreyCoderK <leshodaimekossonou@gmail.com>
2019-06-12 18:30:33 +0200

    quest 04

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,11 +3,16 @@ package piscine
 import "math"
 
 func RecursiveFactorial(x int) int {
-	if 0 == x || 1 == x {
-		return 1
-	} else if x*RecursiveFactorial(x-1) > math.MaxInt32 || x < 0 {
+	if x < 0 || x > math.MaxInt32 {
 		return 0
+	} else if 0 == x || 1 == x {
+		return 1
 	} else {
-		return x * RecursiveFactorial(x-1)
+		res := x*RecursiveFactorial(x-1) > math.MaxInt32
+		if res {
+			return x * RecursiveFactorial(x-1)
+		} else {
+			return 0
+		}
 	}
 }
