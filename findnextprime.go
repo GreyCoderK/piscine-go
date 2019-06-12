328c709114a566326144d9983bac6d96bff07649
GreyCoderK <leshodaimekossonou@gmail.com>
2019-06-12 17:55:20 +0200

    next

--- findnextprime.go
+++ findnextprime.go
@@ -1,11 +1,11 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	if piscine.IsPrime(nb) {
+	if IsPrime(nb) {
 		return nb
 	} else {
 		n := nb + 1
-		for piscine.IsPrime(n) == false {
+		for IsPrime(n) == false {
 			n++
 		}
 		return n
