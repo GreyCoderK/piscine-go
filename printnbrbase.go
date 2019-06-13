672d4f4ac96be047f710e936d9791d2168c35369
ARMEL REAL <armelreal@MacBook-Pro-de-ARMEL.local>
2019-06-13 23:45:25 +0000

    quest 05

--- printnbrbase.go
+++ printnbrbase.go
@@ -7,34 +7,32 @@ import (
 
 func PrintNbrBase(nbr int, str string){
 	indx := 0
-	max := ""
 	
 	for _,res:= range str {
 		if string(res) == "-" || string(res) == "+" || strings.Count(str, string(res)) > 1{
 			indx = 1
 			break
 		}
-		if string(res) > max {
-			max = string(res)
-		}
 	}
 	
-	check := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	ind := strings.Index(check, max) + 1
-	if indx == 1 || ind < 2 {
+	if indx == 1 {
 		fmt.Print("NV")	
 	}else{
 		if nbr < 0 {
 			fmt.Print("-")
 			nbr *= -1	
 		}
-		for nbr > ind {
-			if nbr > ind {
-				fmt.Print(nbr % ind)
-				nbr = int(nbr/ind)
+		i:=0
+		for nbr > len(str) {
+			if nbr > len(str) {
+				defer fmt.Print(string(str[nbr % len(str)]))
+				nbr = int(nbr/len(str))
+				i++
 			}
 			
 		}
-		fmt.Print(nbr)
+		
+		defer fmt.Print(string(str[nbr]))
+		
 	}
 }
