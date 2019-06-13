08f0f4bb505395ca19edf88c646f0792c694b898
GreyCoderK <leshodaimekossonou@gmail.com>
2019-06-13 03:49:43 +0200

    quest 05

--- capitalize.go
+++ capitalize.go
@@ -3,5 +3,5 @@ package piscine
 import "strings"
 
 func Capitalize(s string) string {
-	return strings.Title(ToLower(s))
+	return strings.Replace(strings.Title(ToLower(s)), s[strings.Index("_")+1], ToUpper(s[strings.Index("_")+1]))
 }
