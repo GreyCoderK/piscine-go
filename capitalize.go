47fdfc0175d3787d1807c651b2c5a6d579499f41
GreyCoderK <leshodaimekossonou@gmail.com>
2019-06-13 04:03:39 +0200

    quest 05

--- capitalize.go
+++ capitalize.go
@@ -5,7 +5,7 @@ import "strings"
 func Capitalize(s string) string {
 	res := strings.Title(ToLower(s))
 	indx := strings.Index("_")
-	if res == -1 {
+	if indx == -1 {
 		return res
 	} else {
 		res = strings.Replace(res, res[indx+1], ToUpper(res[indx+1]), indx)
