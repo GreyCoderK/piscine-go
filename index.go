409ba4f0ef08cfe512419dfbf73d368ac91a0b7a
GreyCoderK <leshodaimekossonou@gmail.com>
2019-06-13 03:01:08 +0200

    quest 05

--- index.go
+++ index.go
@@ -1,16 +1,7 @@
 package piscine
 
+import "strings"
+
 func Index(s string, toFind string) int {
-	cmp := 0
-	res := []rune(s)
-	for i := 0; i < len(res); i++ {
-		if toFind == string(res[i]) {
-			cmp++
-		}
-	}
-	if cmp == 0 {
-		return -1
-	} else {
-		return cmp
-	}
+	return strings.Index(s, toFind)
 }
