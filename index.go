32b76903df7a93dd6b18f07de90b71f8f155d3f4
GreyCoderK <leshodaimekossonou@gmail.com>
2019-06-13 02:48:29 +0200

    quest 05

---
+++ index.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	cmp := 0
+	res := []rune(s)
+	for i := 0; i < len(s); i++ {
+		if toFind == int(s[i]) {
+			cmp++
+		}
+	}
+	if cmp == 0 {
+		return -1
+	} else {
+		return cmp
+	}
+}
