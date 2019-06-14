91d87cded0e070904f97e0b2f584f03e405e02eb
ARMEL REAL <armelreal@MacBook-Pro-de-ARMEL.local>
2019-06-14 03:17:09 +0000

    quest 05

--- printnbrbase.go
+++ printnbrbase.go
@@ -1,11 +1,19 @@
 package piscine
 
-import (
+import(
 	"fmt"
 	"strings"
 )
 
-func PrintNbrBase(nbr int, str string)(){
+func Reverse(s string) string {
+    var reverse string
+    for i := len(s)-1; i >= 0; i-- {
+        reverse += string(s[i])
+    }
+    return reverse 
+}
+
+func PrintNbrBase(nbr int64, str string)(){
 	indx := 0
 	
 	for _,res:= range str {
@@ -14,25 +22,27 @@ func PrintNbrBase(nbr int, str string)(){
 			break
 		}
 	}
-	
-	if indx == 1 {
-		fmt.Print("NV")	
+	if math.MaxInt64 < nbr || math.MinInt64 > nbr{
+		fmt.Println(nbr)
+	}else if indx == 1 || len(str) <= 2{
+		fmt.Println("NV")	
 	}else{
 		if nbr < 0 {
 			fmt.Print("-")
 			nbr *= -1	
 		}
 		i:=0
-		for nbr > len(str) {
-			if nbr > len(str) {
-				defer fmt.Print(string(str[nbr % len(str)]))
-				nbr = nbr/len(str)
+		nan:=""
+		for nbr >= int64(len(str)) {
+			if nbr >= int64(len(str)) {
+				nan +=string(str[nbr % int64(len(str))])
+				nbr = nbr/int64(len(str))
 				i++
 			}
 			
 		}
 		
-		defer fmt.Print(string(str[nbr]))
-		
+		nan +=string(str[int64(nbr)])
+		fmt.Println(Reverse(nan))
 	}
 }
