4db0d6764dac5d531d863e5526092990cf9268b1
GreyCoderK <leshodaimekossonou@gmail.com>
2019-06-16 00:59:45 +0200

    quest 07

--- appendrange.go
+++ appendrange.go
@@ -1,15 +1,15 @@
 package piscine
 
 func AppendRange(min, max int) []int {
-	if int(min) >= int(max) {
-		var tab []int = nil
-		return tab
-	}else{
-		tab := []int{}
-		for i:= int(min);int(min) <int(max);i++ {
-			tab = append(tab,int(i))
-		}
-		return tab
-	}
+        if min >= max {
+                var tab []int = nil
+                return tab
+        }else{
+                tab := []int{}
+		
+                for i:= min;i < max;i++ {
+                        tab = append(tab,i)
+                }
+                return tab
+        }
 }
-
