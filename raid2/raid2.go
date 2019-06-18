ae6778564ed19ccd68118880bf51d63fef14ff73
greycoderk <leshodaimekossonou@gmail.com>
2019-06-18 19:04:17 +0000

    raid2

--- raid2/raid2.go
+++ raid2/raid2.go
@@ -186,22 +186,6 @@ func printBoard(board [9][9]int) {
 	}
 }
 
-func parseInput(input string) [9][9]int {
-	board := [9][9]int{}
-	scanner := bufio.NewScanner(strings.NewReader(input))
-
-	scanner.Split(bufio.ScanRunes)
-
-	for row := 0; row < 9; row++ {
-		for col := 0; col < 9; col++ {
-			scanner.Scan()
-			i1, _ := strconv.Atoi(scanner.Text())
-			board[row][col] = i1
-		}
-	}
-	return board
-}
-
 func iscorrectparam(param string) bool{
 	board := parseInput(param)
 	if len(param)!=81{
