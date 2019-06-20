c979b88d5ddacabf469d782029ec0e2d758afca3
greycoderk <leshodaimekossonou@gmail.com>
2019-06-20 20:52:48 +0000

    quest 08

--- displayfile/main.go
+++ displayfile/main.go
@@ -15,7 +15,7 @@ func main(){
 		file, err:= os.Open(string(args[0]))
 		
 		if err != nil {
-			fmt.Print(err.Error())
+			fmt.Printf("[%q]", strings.Trim(err.Error(), "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\n"))
 		}
 		arr := make([]byte,14)
 		file.Read(arr)
