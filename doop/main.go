7e18ac44b778c145214b2381a8a154ea83563162
greycoderk <leshodaimekossonou@gmail.com>
2019-06-21 15:00:55 +0000

    quest 09

--- doop/main.go
+++ doop/main.go
@@ -24,28 +24,25 @@ func main(){
 		if validateOperator(args[1]) == false {
 			fmt.Println(0)
 		}else{
-			premier, err1 := strconv.Atoi(args[0])
-			second, err2 :=	strconv.Atoi(args[2])
-
-			if err1 != nil || err2 != nil {
-				fmt.Println(0)
+			premier, _ := strconv.Atoi(args[0])
+			second, _ := strconv.Atoi(args[2])
+			
+			if args[1] == "%" && second == 0 {
+				fmt.Print("No Modulo by 0\n")
+			}else if args[1] == "/" && second == 0 {
+				fmt.Print("No division by 0\n")
+			}else if args[1] == "+" {
+				fmt.Println(premier+second)
+			}else if args[1] == "-" {
+				fmt.Println(premier-second)
+			}else if args[1] == "*" {
+				fmt.Println(premier * second)
+			}else if args[1] == "/" {
+				fmt.Println(premier/second)
 			}else{
-				if args[1] == "%" && second == 0 {
-					fmt.Print("No Modulo by 0\n")
-				}else if args[1] == "/" && second == 0 {
-					fmt.Print("No division by 0\n")
-				}else if args[1] == "+" {
-					fmt.Println(premier+second)
-				}else if args[1] == "-" {
-					fmt.Println(premier-second)
-				}else if args[1] == "*" {
-					fmt.Println(premier * second)
-				}else if args[1] == "/" {
-					fmt.Println(premier/second)
-				}else{
-					fmt.Println(premier%second)
-				}
+				fmt.Println(premier%second)
 			}
+
 		}
 	}
 
