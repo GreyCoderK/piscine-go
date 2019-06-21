df4e890115d85d1cd3a6eebae3445908ff132fc0
greycoderk <leshodaimekossonou@gmail.com>
2019-06-21 12:33:18 +0000

    quest 09

--- issorted.go
+++ issorted.go
@@ -3,7 +3,7 @@ package piscine
 func IsSorted(f func(a, b int) int, tab []int) bool {
 	ascendant, descendant:= true,true
 	for i:= 0; i < len(tab) - 1; i++{
-		if f(tab[i],tab[i+1]) =< 0 {
+		if f(tab[i],tab[i+1]) <= 0 {
 			descendant = false	
 		}
 	}
