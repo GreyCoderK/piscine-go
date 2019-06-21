d86943457157e0d24ac362c34fae3044608aae5e
GreyCoderK <46757795+GreyCoderK@users.noreply.github.com>
2019-06-21 00:31:18 +0000

    Update issorted.go

--- issorted.go
+++ issorted.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsSorted(f func(a, b int) int, tab []int) bool {
 	for i:= 0; i < len(tab) - 1; i++{
-		if f(tab[i],tab[i+1]) == 1 {
+		if f(tab[i],tab[i+1]) == -1 {
 			return true	
 		}
 	}
