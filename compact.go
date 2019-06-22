45b488a89350f5c0127387aa8ca9a0485cfc6538
greycoderk <leshodaimekossonou@gmail.com>
2019-06-22 10:08:21 +0000

    Hackaton

--- compact.go
+++ compact.go
@@ -3,9 +3,9 @@ package piscine
 func Compact(ptr *[]string, length int) int {
 	cmp := 0
 	for_, res := range ptr {
-		if *(res) != " " {
-			cmp++
+		if *(res) == " " {
+			length -= 1
 		}
 	}
-	return cmp
+	return length
 }
