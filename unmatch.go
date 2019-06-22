ef556a264386534392cb0a0bd7d14730813a9dbf
greycoderk <leshodaimekossonou@gmail.com>
2019-06-22 11:00:51 +0000

    Hackaton

---
+++ unmatch.go
@@ -0,0 +1,16 @@
+package piscine
+
+func Unmatch(arr []int) int {
+	for _,res:= range arr {
+		fois:= 0
+		for _,el:= range arr {
+			if el == res {
+				fois++
+			}
+		}
+		if fois == 1 {
+			return res
+		}
+	}
+	return -1
+}
