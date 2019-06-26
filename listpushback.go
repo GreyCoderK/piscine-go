a1006243cd9daaae364452b7dcfa77a98f10f201
greycoderk <leshodaimekossonou@gmail.com>
2019-06-26 23:47:46 +0000

    quest 11

--- listpushback.go
+++ listpushback.go
@@ -12,12 +12,13 @@ type List struct {
 
 func ListPushBack(l *List, data interface{}) {
 	n:=&NodeL{Data:data}
-	if l.Head==nil{
-		l.Head=n
-	}else if l.Tail==nil{
-		l.Tail=n
-	}else{
-		tail := l.Tail
-		tail.Next=n
+        if l.Head==nil{
+                l.Head=n
+        }else if l.Tail==nil{
+                head := l.Head
+		head.Next = n
+		l.Tail = n 
+        }else{
+		l.Tail.Next = n
 	}
 }
