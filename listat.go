0f460e99649e41e2d122319fc499b2d7ecd3e125
greycoderk <leshodaimekossonou@gmail.com>
2019-06-27 13:42:04 +0000

    quest 11

--- listat.go
+++ listat.go
@@ -2,20 +2,16 @@ package piscine
 
 func ListAt(l *NodeL, pos int) *NodeL{
 	if l == nil {
-		return nil 
+		return nil
 	}
-	if pos == 1 {
-		return l
-	}else{
-		cmpt := 1
-		current := l
-		for current.Next != nil {
-			current = current.Next
-			cmpt++
-			if cmpt == pos {
-				return current
-			}
+	current := l
+	index := 0
+	for index < pos {
+		if current.Next == nil {
+			return nil
 		}
+		current = current.Next
+		index++
 	}
-	return nil
+	return current
 }
