d26e88a14743030fffb3e4d77962f411ea386a39
greycoderk <leshodaimekossonou@gmail.com>
2019-06-27 15:08:49 +0000

    quest 11

---
+++ listforeach.go
@@ -0,0 +1,29 @@
+package piscine
+
+func ListForEach(l *List, f func(*NodeL)) {
+	if l.Head != nil {
+		current := l.Head
+		for current != nil {
+			f(current)
+			current = current.Next
+		}
+	}
+}
+
+func Add2_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) + 2
+	case string:
+		node.Data = node.Data.(string) + "2"
+	}
+}
+
+func Subtract3_node(node *NodeL) {
+	switch node.Data.(type) {
+	case int:
+		node.Data = node.Data.(int) - 3
+	case string:
+		node.Data = node.Data.(string) + "-3"
+	}
+}
