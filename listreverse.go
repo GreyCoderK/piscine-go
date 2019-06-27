27077900a1928a4ac1e8c4e65746d619b8dbf939
greycoderk <leshodaimekossonou@gmail.com>
2019-06-27 14:49:06 +0000

    quest 11

--- listreverse.go
+++ listreverse.go
@@ -2,15 +2,16 @@ package piscine
 
 func ListReverse(l *List) {
 	current := l.Head
-        var precedent =  &NodeL{}
+	var prev *NodeL
 
-        for current != nil { 
-                next := current.Next
-                current.Next = precedent
-                precedent = current
-                current = next
-        }
-        temp := l.Head
+	for current != nil {
+		next := current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+
+	temp := l.Head
 	l.Head = l.Tail
 	l.Tail = temp
 }
