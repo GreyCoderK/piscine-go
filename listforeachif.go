483fbefb90a6abaaf108485bba212b237e7a3a66
greycoderk <leshodaimekossonou@gmail.com>
2019-06-27 15:22:07 +0000

    quest 11

---
+++ listforeachif.go
@@ -0,0 +1,39 @@
+func IsPositive_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func IsNegative_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	case string, rune:
+		return false
+	}
+	return false
+}
+
+func IsNotNumeric_node(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return false
+	case string, rune:
+		return true
+	}
+	return true
+}
+
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+	current = l.head
+	for current != nil {
+		if cond(current) {
+			f(current)
+		}
+		current = current.Next
+	}
+}
