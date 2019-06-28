b00bae04f4368638636b726efb98caf44fb6301a
greycoderk <leshodaimekossonou@gmail.com>
2019-06-28 15:28:25 +0000

    quest 11

--- listsort.go
+++ listsort.go
@@ -6,29 +6,30 @@ type NodeI struct {
 }
 
 func ListSort(l *NodeI) *NodeI {
-	if l == nil {
+        if l == nil {
 		return l
 	}
-
 	current := l
         cmpt := 0
 	next := l.Next
-	var temp *NodeI
 	
-	if next == nil{
-		return  l 
-	}
+	if next == nil {
+		return current
+	} 
+	
+	var first,temp *NodeI	
 
         for current != nil {
 		if cmpt == 0 {
+			first = current
 			cmpt++
+			temp = next.Next
 		}
 		if cmpt > 0 {
 			if temp == nil {
-				return current
+				return first
 			}else{
-				current = current.Next
-				next = current.Next
+				next = temp
 			}
 		}
 		for next != nil {
@@ -37,7 +38,8 @@ func ListSort(l *NodeI) *NodeI {
 			}
                         next = next.Next
                 }
+                current = current.Next
 		temp = current.Next
         }
-	return current
+	return first
 }
