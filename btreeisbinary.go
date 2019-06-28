9054b5067c9dbb4e2734f971a2cf5254e451fdaf
greycoderk <leshodaimekossonou@gmail.com>
2019-06-28 18:03:59 +0000

    quest 12

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -4,12 +4,11 @@ func BTreeIsBinary(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
-	
 	if root.Left != nil {
-		return root.Left.Data < root.Data && BTreeIsBinary(root.Left)
+		return ( root.Left.Data < root.Data )  && BTreeIsBinary(root.Left)
 	}
 	if root.Right != nil {
-                return root.Right.Data > root.Data && BTreeIsBinary(root.Right)
+                return ( root.Right.Data > root.Data ) && BTreeIsBinary(root.Right)
         }
 	return false
 
