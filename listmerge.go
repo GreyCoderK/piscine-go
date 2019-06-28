f17b681de231db18ad8ae64e9430b68e12924bac
greycoderk <leshodaimekossonou@gmail.com>
2019-06-28 12:55:39 +0000

    quest 11

--- listmerge.go
+++ listmerge.go
@@ -1,6 +1,13 @@
 package piscine
 
 func ListMerge(l1, l2 *List) {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
 	current := l1.Head
 	for current.Next != nil {
 		current = current.Next
