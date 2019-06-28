ad95813d49eea8aefdc7a0e30efd76e2995efdbc
greycoderk <leshodaimekossonou@gmail.com>
2019-06-28 01:30:08 +0000

    quest 12

---
+++ btreeisbinary.go
@@ -0,0 +1,15 @@
+package piscine
+
+func BTreeIsBinary(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	
+	if root.Left != nil {
+		return root.Left.Data < root.Data && BTreeIsBinary(root.Left)
+	}else if root.Left != nil {
+                return root.Right.Data > root.Data && BTreeIsBinary(root.Right)
+        }else{
+		return false
+	}
+}
