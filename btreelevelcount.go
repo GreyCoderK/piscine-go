2a6e0fc7dd60a4c92d7c04b8a5423f07fdb28624
greycoderk <leshodaimekossonou@gmail.com>
2019-06-28 00:33:10 +0000

    quest 12

---
+++ btreelevelcount.go
@@ -0,0 +1,16 @@
+package piscine
+
+func BTreeLevelCount(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	gauche := BTreeLevelCount(root.Left)
+	droite := := BTreeLevelCount(root.Right)
+
+	if droite > gauche {
+		return droite + 1
+	}else{
+		return gauche + 1
+	}
+}
