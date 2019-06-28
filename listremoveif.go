e8203010db94f07e0111b6c53cc6c06028c7c342
greycoderk <leshodaimekossonou@gmail.com>
2019-06-28 12:42:03 +0000

    quest 11

--- listremoveif.go
+++ listremoveif.go
@@ -1,22 +1,23 @@
 package piscine
 
 func ListRemoveIf(l *List, data_ref interface{}) {
-	if l != nil {
-		current := l.Head
-		var previous *NodeL = nil
+	if l == nil {
+		return
+	}
+	current := l.Head
+	var previous *NodeL = nil
  
-		for current != nil {
-			if current.Data == data_ref {
-				if previous == nil {
-					previous = l.Head 	
-				}else{
-					previous = current
-					previous.Next = current.Next
-				}
+	for current != nil {
+		if current.Data == data_ref {
+			if previous == nil {
+				l.Head = current.Next 	
 			}else{
 				previous = current
+				previous.Next = current.Next
 			}
-			current = current.Next	
-		}		
+		}else{
+			previous = current
+		}
+		current = current.Next			
 	}
 }
